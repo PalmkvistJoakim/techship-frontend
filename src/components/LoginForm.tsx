3b875d7cf3df63ab1fba0e9f1c93088225831140
PalmkvistJoakim <jockepalmkvist@gmail.com>
2022-10-16 19:45:13 +0200

    Added login & searchbar

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,5 +1,51 @@
+import styled from "styled-components";
+
 const LoginForm = () => {
-  return <h1>Login Page</h1>;
+  return (
+    <Container className="container">
+      <Form>
+        <label htmlFor="html"></label>
+        <Input type="text" placeholder="Username" />
+        <label htmlFor="psw"></label>
+        <Input type="password" placeholder="Password" />
+
+        <Button type="submit">Logga in</Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default LoginForm;
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Form = styled.form`
+  border: 1px solid white;
+  width: 30%;
+  height: 50%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  display: flex;
+  width: 50%;
+  padding: 10px;
+  margin-left: 130px;
+`;
+
+const Button = styled.button`
+  display: flex;
+  width: 20%;
+  padding: 10px;
+  margin-top: 30px;
+  margin-left: 220px;
+  border-radius: 20px;
+`;
