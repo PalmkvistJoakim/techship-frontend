dcff3d1886d3ac1a0187d07ab25ab33c157fd9bc
PalmkvistJoakim <jockepalmkvist@gmail.com>
2022-10-30 18:10:52 +0100

    fixalot

--- src/components/common/TableHeader.tsx
+++ src/components/common/TableHeader.tsx
@@ -68,18 +68,3 @@ function TableHeader({ columns }: Props) {
 }
 
 export default TableHeader;
-
-const Th = styled.th`
-  text-align: left;
-  margin: 2px;
-  padding: 4px;
-  margin-left: 6px;
-  cursor: pointer;
-`;
-
-const Tr = styled.tr`
-  display: grid;
-  grid-template-columns: 36px 36px 244px 248px 184px 178px;
-  border-collapse: collapse;
-  border: 1px solid;
-`;
