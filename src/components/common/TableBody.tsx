cf3d53bffc08869ae3cb45eeed4d2533cfb0e340
januex <jankenmatene933@gmail.com>
2022-11-21 17:23:29 +0100

    fix

--- src/components/common/TableBody.tsx
+++ src/components/common/TableBody.tsx
@@ -175,7 +175,7 @@ const Tr = styled.tr<BdgColor>`
   grid-template-areas:
     "mail name comment"
     "mail created stage";
-  grid-template-columns: 1rem 18rem 6rem;
+  grid-template-columns: 1rem 14rem 10rem;
   grid-template-rows: 1fr 1fr;
   justify-items: stretch;
   margin-top: 80px;
